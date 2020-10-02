b1ff2e47fd1abf81a76ed76d73a1a01c2d8a4775
Alistair Norman <alistair@super.gd>
2020-10-02 13:33:58 -0700

    Fill out an extremely simple game show page
    This makes an api request to get the game data then parses the data and
    displays goals, shots, and hits for each team.

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,19 @@
 class GamesController < ApplicationController
   def show
+    response = JSON.parse(Faraday.get("https://statsapi.web.nhl.com/api/v1/game/#{params[:id]}/boxscore").body)
+
+    @away_team = {
+      name: response.dig("teams", "away", "team", "name"),
+      goals: response.dig("teams", "away", "teamStats", "teamSkaterStats", "goals"),
+      shots: response.dig("teams", "away", "teamStats", "teamSkaterStats", "shots"),
+      hits: response.dig("teams", "away", "teamStats", "teamSkaterStats", "hits"),
+    }
+
+    @home_team = {
+      name: response.dig("teams", "home", "team", "name"),
+      goals: response.dig("teams", "home", "teamStats", "teamSkaterStats", "goals"),
+      shots: response.dig("teams", "home", "teamStats", "teamSkaterStats", "shots"),
+      hits: response.dig("teams", "home", "teamStats", "teamSkaterStats", "hits"),
+    }
   end
 end
--- spec/requests/games_controller_spec.rb
+++ spec/requests/games_controller_spec.rb
@@ -5,7 +5,44 @@ RSpec.describe "GamesController", type: :request do
     subject { get "/games/#{game_id}" }
 
     let(:game_id) { 2017020001 }
+    let(:response_double) { double }
+
+    before do
+      allow(Faraday)
+        .to receive(:get).with("https://statsapi.web.nhl.com/api/v1/game/2017020001/boxscore")
+        .and_return(response_double)
+
+      allow(response_double)
+        .to receive(:body)
+        .and_return(
+          {
+            "teams" => {
+              "away" => {
+                "team" => { "name" => "Toronto Maple Leafs" },
+                "teamStats" => {
+                  "goals" => 7,
+                  "shots" => 31,
+                  "hits" => 16
+                }
+              },
+              "home" => {
+                "team" => { "name" => "Winnipeg Jets" },
+                "teamStats" => {
+                  "goals" => 2,
+                  "shots" => 37,
+                  "hits" => 18
+                }
+              }
+            }
+          }.to_json
+        )
+    end
 
     it { is_expected.to render_template :show }
+
+    it "gets the stuff" do
+      expect(Faraday).to receive(:get).with("https://statsapi.web.nhl.com/api/v1/game/2017020001/boxscore")
+      subject
+    end
   end
 end
